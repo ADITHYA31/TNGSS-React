0e8df03a916f7b14b58117de35b87ac918de0dd0
Rathee <sagarrathi5@gmail.com>
2025-04-24 01:35:30 +0530

    spacing for mobile

--- src/components/Homepage/Prefooter/ParallelScroll.js
+++ src/components/Homepage/Prefooter/ParallelScroll.js
@@ -35,20 +35,20 @@ export default function ParallelScroll() {
     return (
         <section
             ref={globalpavilion}
-            className="relative min-h-screen  w-screen isolate bg-white h-30vh"
+            className="relative min-h-screen  w-screen isolate bg-white h-60vh"
         >
-            <div className="w-full h-full flex flex-col justify-center overflow-hidden h-30vh">
+            <div className="w-full h-full flex flex-col justify-center overflow-hidden h-60vh">
             <div className="flags flex w-full gap-3 will-change-transform md:flex-nowrap md:overflow-hidden overflow-x-auto">
   {data.map((item, index) => (
     <div
       key={index}
-      className="w-2/3 md:w-1/2 h-[80vh] bg-[#F3F4F6] rounded-2xl px-4 py-4 shrink-0 h-30vh"
+      className="w-2/3 md:w-1/2 h-[80vh] bg-[#F3F4F6] rounded-2xl px-4 py-4 shrink-0 h-60vh"
     >
       <div
         className="flex flex-col justify-end w-full h-full rounded-2xl bg-cover bg-center"
         style={{
           backgroundImage: `url(${item.img})`,
-          minWidth : '200px'
+          minWidth : '230px'
         }}
       >
         <div className="text-xxl md:text-4xl text-white px-5 pb-6">
