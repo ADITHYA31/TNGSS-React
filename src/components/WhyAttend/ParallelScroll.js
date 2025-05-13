fbf397bff92e9597e8530cd414cef8eeacf94d03
ADITHYA <adithyavatti@gmail.com>
2025-05-13 12:47:15 +0530

    changes

--- src/components/WhyAttend/ParallelScroll.js
+++ src/components/WhyAttend/ParallelScroll.js
@@ -76,10 +76,10 @@ export default function ParallelScroll({ cont = [
         alignItems: 'center',
       }}
     >
-      <div className="w-full h-full flex flex-col md:items-center justify-center overflow-hidden items-center">
+      <div className="w-full h-full flex flex-col justify-center overflow-hidden items-center">
         {/* Mobile View */}
         {isMobile ? (
-          <div className="w-full relative flex flex-col items-center justify-center pb-12">
+          <div className="w-full relative flex flex-col items-center justify-center pb-12 items-center">
 
             {/* Arrows */}
          <button
