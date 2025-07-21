0471c63dafa06d7bb7a872687d1bbf8fb28abf46
ADITHYA <adithyavatti@gmail.com>
2025-07-21 14:50:49 +0530

    uncomment nav links

--- src/components/Elements/NavBar.js
+++ src/components/Elements/NavBar.js
@@ -67,13 +67,13 @@ export default function NavBar() {
           />
         </a>
 
-        {/* <div className=" hidden  lg:absolute left-1/2 md:flex  text-xl transform  gap-4  lg:-translate-x-1/2">
+        <div className=" hidden  lg:absolute left-1/2 md:flex  text-xl transform  gap-4  lg:-translate-x-1/2">
           {menuItems.map((item, index) => (
             <a key={index} href={item.link} className=" hover:underline " >
               {item.name}
             </a>
           ))}
-        </div> */}
+        </div>
 
         {/* Desktop Menu */}
         <div
